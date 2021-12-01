00de19320b55be1177785a966558a39af5f378fa
Jacob Hilker <jacob.hilker2@gmail.com>
2021-12-01 17:16:08 -0500

    added app config, addd power script

--- qtile/.config/qtile/keybinds.py
+++ qtile/.config/qtile/keybinds.py
@@ -36,7 +36,7 @@ keys = [
     Key("M-<Tab>", lazy.next_layout(), desc="Toggle between layouts"),
     Key("M-S-q", lazy.window.kill(), desc="Kill focused window"),
        Key("M-S-r", lazy.restart(), desc="Restart Qtile"),
-    Key("M-C-q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key("M-S-p", lazy.spawn("rofi -show powermenu -modi powermenu:~/.dotfiles/rofi/.config/rofi/scripts/power.sh"), desc="Manage machine power state"),
     Key("M-r", lazy.spawn("rofi -config ~/.dotfiles/rofi/.config/rofi/apps.rasi -show drun"), desc="Spawn a command using a prompt widget"),
     Key("<XF86AudioRaiseVolume>", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +2%"), "Raise Volume"),
     Key("<XF86AudioLowerVolume>", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -2%"), "Lower Volume"),
