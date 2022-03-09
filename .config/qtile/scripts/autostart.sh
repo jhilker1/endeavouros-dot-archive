88e02f907b069037af7435d388af09e4506ed262
Jacob Hilker <jacob.hilker2@gmail.com>
2022-03-09 15:46:47 -0500

    fixed wallpaper loc

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -2,7 +2,7 @@
 ~/.dotfiles/.screenlayout/netbook-366-768.sh
 xset b off
 picom -b 
-feh --bg-center ~/wallpapers/gruvbox/pacman.png
+feh --bg-center ~/Pictures/wallpapers/gruvbox/pacman.png
 redshift -l $(curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | jq -r '"\(.location.lat):\(.location.lng)"') &
 emacs --daemon &
 emacs --with-profile=doom --daemon &
