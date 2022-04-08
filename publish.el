d38f9ea8dcf4248d869e44151a0e20ad17eaf91b
Jacob Hilker <jacob.hilker2@gmail.com>
2022-04-08 17:43:54 -0400

    started hugo site for dots

---
+++ publish.el
@@ -0,0 +1,66 @@
+;;; publish.el --- Publishes my dotfiles to hugo markdown -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2022 Jacob Hilker
+;;
+;; Author: Jacob Hilker <jacob.hilker2@gmail.com>
+;; Maintainer: Jacob Hilker <jacob.hilker2@gmail.com>
+;; Created: April 08, 2022
+;; Modified: April 08, 2022
+;; Version: 0.0.1
+;; Keywords: publishing hugo dotfiles
+;; Homepage: https://github.com/jhilker1/dots
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;; Code:
+(setq user-emacs-directory (expand-file-name "./.packages")
+      make-backup-files nil) ;;; Using git, therefore cringe
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package '(org :type built-in))
+(straight-use-package '(ox-hugo
+                        :host github :repo "kaushalmodi/ox-hugo"))
+
+(setq org-hugo-paired-shortcodes "highlight")
+
+(defun build/export-configs ()
+  (dolist (org-file (directory-files-recursively ".config" "\.org$"))
+    (with-current-buffer (find-file org-file)
+      (message (format "[build] Exporting %s" org-file))
+      (org-hugo-export-wim-to-md :all-subtrees nil nil nil))))
+
+(defun build/export-pages ()
+  (dolist (org-file (directory-files "."))
+    (if (equal "org" (file-name-extension org-file))
+    (with-current-buffer (find-file org-file)
+      (message (format "[build] Exporting %s" org-file))
+      (org-hugo-export-wim-to-md :all-subtrees nil nil nil)))))
+
+(defun build/export-all ()
+  (build/export-pages)
+  (build/export-configs))
+
+
+(build/export-all)
+(provide 'publish)
+
+
+(provide 'publish)
+;;; publish.el ends here
